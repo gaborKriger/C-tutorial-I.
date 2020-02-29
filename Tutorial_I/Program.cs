3db48a1c2e40fb4eeb59c23be6a57a884fe97f98
Kriger Gábor <regirck@gmail.com>
2020-02-29 14:39:12 +0100

    Add project files.

---
+++ Tutorial_I/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Tutorial_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            uint i = 2;
+            Console.WriteLine(i);
+
+            Console.WriteLine(uint.MinValue);
+            Console.WriteLine(uint.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(int.MaxValue);
+
+            Console.ReadKey();
+        }
+    }
+}
